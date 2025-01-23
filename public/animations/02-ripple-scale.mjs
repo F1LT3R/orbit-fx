db1ae1c4d1fcb7feafb25472032e5c765df83e39
F1LT3R <F1LT3R@users.noreply.github.com>
2025-01-22 23:42:32 -0500

    Clean up bowl ripple.

--- public/animations/02-ripple-scale.mjs
+++ public/animations/02-ripple-scale.mjs
@@ -4,10 +4,10 @@ import center from '/helpers/center.mjs'
 
 const $boxes = document.querySelectorAll('.box')
 
-let speed = 4
+let speed = 20
 
 const animation = new Animation(60)
-const timeline = animation.timeline('anim1', 0, 100, speed, false, (timeline) => {
+const timeline = animation.timeline('anim1', 0, 1000, speed, false, (timeline) => {
     timeline.speed = -speed
 })
 
@@ -15,17 +15,18 @@ const rnd = () => -90 + Math.floor(Math.random() * 180)
 
 const winCenterX = window.innerWidth / 2
 const winCenterY = window.innerHeight / 2
+const rippleRatio = Math.sqrt(window.innerWidth * window.innerHeight) / 1.25
 
 $boxes.forEach(($box, index) => {
     const boxCenter = center($box)
     const distance = dist(boxCenter.x, boxCenter.x, winCenterX, winCenterY)
-    const delay = (60 / window.innerWidth) * distance
+    const delay = (600 / rippleRatio) * distance
 
     /* prettier-ignore */
     timeline.actor(`box-${index}`, css3D($box))
 		.track('translateZ')
 			.key(delay, 0, 'inOutSine')
-			.key(100, -100)
+			.key(1000, -100)
 })
 
 animation.load('anim1')
